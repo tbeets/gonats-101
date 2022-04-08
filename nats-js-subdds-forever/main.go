ec2bf75844a86442bd8399b002f361b54ecf35bd
Todd Beets <todd@beetsme.net>
2022-04-07 17:14:18 -0700

    Add a Pull JS subscribe forever (tight loop around fetch)

---
+++ nats-js-subdds-forever/main.go
@@ -0,0 +1,164 @@
+// Copyright 2012-2021 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"time"
+
+	"github.com/nats-io/nats.go"
+)
+
+func usage() {
+	log.Printf("Usage: nats-js-subdds-forever [-s server] [-creds file] [-nkey file] [-tlscert file] [-tlskey file] [-tlscacert file] [-bs batchsize] <stream> <consumer>\n")
+	flag.PrintDefaults()
+}
+
+func showUsageAndExit(exitcode int) {
+	usage()
+	os.Exit(exitcode)
+}
+
+func printMsg(m *nats.Msg, i int) {
+	md, err := m.Metadata()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("[%d]: %s\nSeqPair: [%v] Pending: [%d]", i, m.Subject, md.Sequence, md.NumPending)
+}
+
+func main() {
+	var urls = flag.String("s", nats.DefaultURL, "The nats server URLs (separated by comma)")
+	var userCreds = flag.String("creds", "", "User Credentials File")
+	var nkeyFile = flag.String("nkey", "", "NKey Seed File")
+	var tlsClientCert = flag.String("tlscert", "", "TLS client certificate file")
+	var tlsClientKey = flag.String("tlskey", "", "Private key file for client certificate")
+	var tlsCACert = flag.String("tlscacert", "", "CA certificate to verify peer against")
+	var showHelp = flag.Bool("h", false, "Show help message")
+	var batchSize = flag.Int("bs", 1, "fetch batch size (default 1)")
+
+	log.SetFlags(0)
+	flag.Usage = usage
+	flag.Parse()
+
+	if *showHelp {
+		showUsageAndExit(0)
+	}
+
+	args := flag.Args()
+	if len(args) != 2 {
+		showUsageAndExit(1)
+	}
+
+	// Connect Options.
+	opts := []nats.Option{nats.Name("NATS Sample JS Subscriber")}
+	opts = setupConnOptions(opts)
+
+	if *userCreds != "" && *nkeyFile != "" {
+		log.Fatal("specify -seed or -creds")
+	}
+
+	// Use UserCredentials
+	if *userCreds != "" {
+		opts = append(opts, nats.UserCredentials(*userCreds))
+	}
+
+	// Use TLS client authentication
+	if *tlsClientCert != "" && *tlsClientKey != "" {
+		opts = append(opts, nats.ClientCert(*tlsClientCert, *tlsClientKey))
+	}
+
+	// Use specific CA certificate
+	if *tlsCACert != "" {
+		opts = append(opts, nats.RootCAs(*tlsCACert))
+	}
+
+	// Use Nkey authentication.
+	if *nkeyFile != "" {
+		opt, err := nats.NkeyOptionFromSeed(*nkeyFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		opts = append(opts, opt)
+	}
+
+	// Connect to NATS
+	nc, err := nats.Connect(*urls, opts...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Close()
+
+	// Create JetStream Context from NATS connection
+	js, err := nc.JetStream()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	str, con := args[0], args[1]
+
+	// Simple Pull Consumer
+	// Prefer the Bind subscription option to explicitly identify the JetStream and JS Consumer
+	// and fail if not found.
+	// Note: Redundantly, the durable parameter must be passed (JS Consumer name) even with Bind opt
+	sub, err := js.PullSubscribe("", con, nats.Bind(str, con), nats.ManualAck())
+
+	for {
+		msgs, err := sub.Fetch(
+			*batchSize,
+			nats.MaxWait(5*time.Second))
+
+		var atLeastOne bool
+		for i, msg := range msgs {
+			printMsg(msg, i)
+			err = msg.Ack()
+			if err != nil {
+				log.Fatal(err)
+			}
+			atLeastOne = true
+		}
+
+		if err := nc.LastError(); err != nil {
+			log.Printf("%s", err)
+		}
+
+		if !atLeastOne {
+			// log.Printf("no messages")
+		}
+	}
+}
+
+func setupConnOptions(opts []nats.Option) []nats.Option {
+	totalWait := 10 * time.Minute
+	reconnectDelay := time.Second
+
+	opts = append(opts, nats.ReconnectWait(reconnectDelay))
+	opts = append(opts, nats.MaxReconnects(int(totalWait/reconnectDelay)))
+	opts = append(opts, nats.DisconnectErrHandler(func(nc *nats.Conn, err error) {
+		if err != nil {
+			log.Printf("Disconnected due to:%s, will attempt reconnects for %.0fm", err, totalWait.Minutes())
+		}
+	}))
+	opts = append(opts, nats.ReconnectHandler(func(nc *nats.Conn) {
+		log.Printf("Reconnected [%s]", nc.ConnectedUrl())
+	}))
+	opts = append(opts, nats.ClosedHandler(func(nc *nats.Conn) {
+		if nc.LastError() != nil {
+			log.Printf("ClosedHandler: %v", nc.LastError())
+		}
+	}))
+	return opts
+}
